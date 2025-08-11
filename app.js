ca0867922351651ae41c36f0db68124b96e6eeaa
jlmilaan <jlhmilan@gmail.com>
2025-08-10 18:54:48 -0600

    Add: add friend feature

--- app.js
+++ app.js
@@ -1,1 +1,19 @@
 // El principal objetivo de este desafío es fortalecer tus habilidades en lógica de programación. Aquí deberás desarrollar la lógica para resolver el problema.
+
+let friendsList = [];
+
+function addFriend() {
+    let name = document.querySelector('#friend').value;
+
+    if (name.trim().length == 0) {
+        alert("Por favor, inserte un nombre.");
+    } else {
+        friendsList.push(name);
+    }
+    clear();   
+}
+
+function clear(){
+    let name = document.querySelector('#friend');
+    name.value = ""
+}
